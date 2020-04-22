6dbf575e4ce1f1f9d5fb4333396d597a636e4aa5
Oriol Gual <oriolgual@users.noreply.github.com>
2020-04-22 11:44:37 +0200

    Fix migration (#112)
    Co-authored-by: Txus <me@txus.io>

--- priv/repo/migrations/20200422080410_format_website_url.exs
+++ priv/repo/migrations/20200422080410_format_website_url.exs
@@ -3,9 +3,10 @@ defmodule Regalocal.Repo.Migrations.FormatWebsiteUrl do
   import Ecto.Changeset, only: [change: 2]
   alias Regalocal.Admin.Business
   alias Regalocal.Repo
+  import Ecto.Query
 
   def change do
-    businesses = Repo.all(Business)
+    businesses = Repo.all(from(b in Business, where: not is_nil(b.website)))
 
     Enum.each(businesses, fn business ->
       website =
